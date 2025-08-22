a90c34d21b9ecf2090425e510a05bebee7aa4e48
Sandip Pathe <sandippathe9689@gmail.com>
2025-08-22 09:25:47 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/subscribe/page.tsx
+++ src/app/subscribe/page.tsx
@@ -4,7 +4,6 @@ import * as React from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
-import { CheckIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
   Form,
